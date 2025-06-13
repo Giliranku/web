2aed752fcda711a13b42a2ad7f1f3473c78661b9
LyneXJes <jesselynlaurencia@gmail.com>
2025-06-13 14:03:44 +0700

    add news user

---
+++ app/Livewire/Pages/NewsUserDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages;
+
+use Livewire\Component;
+
+class NewsUserDetail extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.news-user-detail');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Pages\Home;
 use App\Livewire\Pages\ContactUs;
 use App\Livewire\Pages\NewsUser;
+use App\Livewire\Pages\NewsUserDetail;
 
 
 Route::get('/', Home::class);
 Route::get('/contact-us', ContactUs::class);
 Route::get('/news', NewsUser::class);
+Route::get('/news-detail', NewsUserDetail::class);
 
