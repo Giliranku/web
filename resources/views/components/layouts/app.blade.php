f52e60e16c8539b2c4f43acc81b2a8254c7bf93d
Fritz Gradiyoga <slavs223@gmail.com>
2025-06-13 10:05:13 +0700

    Merge Latest Changes
    Perubahan Pertama
    Tanggal : 13 / 06 / 2025

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
-      x-data
-      :data-bs-theme="$store.themeSwitcher.theme">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data :data-bs-theme="$store.themeSwitcher.theme">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,15 +10,12 @@
     @assets
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital,wght@0,300..700;1,300..700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inclusive+Sans:ital,wght@0,300..700;1,300..700&display=swap"
+            rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     @endassets
 
-    @vite([
-        'resources/sass/app.scss',
-        'resources/js/app.js',
-        'resources/css/main.css'
-    ])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/main.css'])
 
     <!-- Alpine store for theme state -->
     <script>
@@ -57,4 +53,5 @@
         @endpersist
     </div>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Livewire\Pages\Home;
-
+use App\Livewire\MyNewPage;
+use App\Livewire\TiketEcommerce;
 
 Route::get('/', Home::class);
+
+Route::get('/newPage', MyNewPage::class)->name('my-new-page');
+
+Route::get('/tiketEcommerce', TiketEcommerce::class)->name('tiket-ecommerce');
 
