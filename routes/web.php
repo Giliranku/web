e8a42f6cb6ce433d2fb90c1088474191cfa5d243
Fritz Gradiyoga <slavs223@gmail.com>
2025-07-01 15:40:28 +0700

    Halaman Cart
    Integrasi Frontend halaman cart pada bagian checkout system

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,16 @@ use Illuminate\Support\Facades\Route;
 use App\Livewire\Pages\Home;
 use App\Livewire\MyNewPage;
 use App\Livewire\TiketEcommerce;
+use App\Livewire\CartPage;
+use App\Livewire\CartPage2;
 
 Route::get('/', Home::class);
 
 Route::get('/newPage', MyNewPage::class)->name('my-new-page');
 
 Route::get('/tiketEcommerce', TiketEcommerce::class)->name('tiket-ecommerce');
+
+Route::get('/cartPage', CartPage::class)->name('cart-page');
+
+Route::get('/cartPage2', CartPage2::class)->name('cart-page2');
 
