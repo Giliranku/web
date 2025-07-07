95013f025156733eebcf187c790025044130e3ae
F2LX <felixfebryan16@gmail.com>
2025-07-07 09:19:43 +0700

    Remove unused file and styling

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use App\Livewire\Admin\ManageNewsEdit;
 use App\Livewire\Pages\restaurantQueueDetail;
 use App\Livewire\Pages\WahanaQueueDetail;
 use App\Livewire\Pages\PriorityQueue;
-
 use App\Livewire\Pages\LoginPage;
 use App\Livewire\Pages\RegisterPage;
 use App\Livewire\Pages\InvoicePage  ;
@@ -31,9 +30,6 @@ Route::get('/news-detail', NewsUserDetail::class);
 Route::get('/manage-news', ManageNews::class);
 Route::get('/manage-news-add', ManageNewsAdd::class);
 Route::get('/manage-news-edit', ManageNewsEdit::class);
-
-Route::post('/post/store', [PostController::class, 'store'])->name('posts.store');
-
 
 Route::get('/search', Sorting::class);
 Route::get('/wahana-details', WahanaDetails::class)->name('wahana.detail');
