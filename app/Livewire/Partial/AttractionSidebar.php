9b987328eb73baf2fbeb8f1952c695f60eba4e32
LyneXJes <jesselynlaurencia@gmail.com>
2025-07-10 14:15:43 +0700

    sidebar staff and backend sidebar

---
+++ app/Livewire/Partial/AttractionSidebar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Partial;
+
+use Livewire\Component;
+
+class AttractionSidebar extends Component
+{
+    public function render()
+    {
+        return view('livewire.partial.attraction-sidebar');
+    }
+}
